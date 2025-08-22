1a5b544e3428210a261e357027e95a964be719e5
shouryasinghsengar-monocept <shouryasinghsengar@monocept.com>
2025-08-23 00:23:31 +0530

    feat: add common file to centralize SVG icons

---
+++ src/components/common/ImportIcons.js
@@ -0,0 +1,17 @@
+import GET_FUND_ICON from "./../../assets/homeScreen/GET_FUND.svg";
+import DEPOSITS_ICON from "./../../assets/homeScreen/DEPOSITS.svg";
+import WITHDRAWALS_ICON from "./../../assets/homeScreen/WITHDRAWALS.svg";
+import ACCOUNTS_ICON from "./../../assets/homeScreen/ACCOUNTS.svg";
+import ADD_ICON from "./../../assets/homeScreen/ADD.svg";
+import GROWTH_ICON from "./../../assets/homeScreen/Growth.svg";
+import PROFILE_IMAGE from "./../../assets/homeScreen/ProfileImage.svg";
+
+export {
+  GET_FUND_ICON,
+  DEPOSITS_ICON,
+  WITHDRAWALS_ICON,
+  ACCOUNTS_ICON,
+  ADD_ICON,
+  GROWTH_ICON,
+  PROFILE_IMAGE,
+};
