2e999de96b0788018d2a73785c24112ee8e268e1
shouryasinghsengar-monocept <shouryasinghsengar@monocept.com>
2025-08-23 10:09:40 +0530

    feat: Add EAS configuration and update metro config for SVG transformer

--- metro.config.js
+++ metro.config.js
@@ -2,13 +2,15 @@ const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
 module.exports = (() => {
+  // Get Expo default config
   const config = getDefaultConfig(__dirname);
 
   // SVG transformer
   config.transformer = {
     ...config.transformer,
-    babelTransformerPath: require.resolve("react-native-svg-transformer/expo"),
+    babelTransformerPath: require.resolve("react-native-svg-transformer"),
   };
+
   config.resolver = {
     ...config.resolver,
     assetExts: config.resolver.assetExts.filter((ext) => ext !== "svg"),
